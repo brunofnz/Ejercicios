9a2e84f68436ae9a7704056c7e4d362c38924a07
Bruno <brunofernandez@protonmail.com>
2020-08-11 23:04:10 -0300

    agregue una linea mas para agregar un producto

--- supermercado.py
+++ supermercado.py
@@ -6,16 +6,40 @@
 
 #El supermercado, del cual se conoce el nombre y la dirección, desea conocer la cantidad total de productos que comercializa y la suma total de los precios de los mismos.
 
+#Gracias a anto, dana, violeta, tomaron la decision sin consultar a los varones de que solamente existe un solo catalogo global xd
 
 #Espacio de clases
 
+ # -*- coding: utf-8 -*-
+
+
+
+from os import system  #esto es para que
+system("cls")        #limpie la consola
+
 class supermercado:
-    def __init__(self,nombre,direccion):
+    def __init__(self,nombre,direccion,pais):
         self.nombre = nombre
         self.direccion = direccion
+        self.catalogo = list()
+        self.__producto = None
+        self.__precio = 0
 
-    def cantidadTotalProductos():
-        pass
+    def __repr__(self):
+        return "{}".format(self.catalogo)
+    
+    def agregarProducto(self, marca, tipo, precio, precioCuidado = False, primeraNecesidad = False):
+        self.__producto = producto(marca, tipo, precio, precioCuidado, primeraNecesidad)
+        self.catalogo.append(self.__producto)
+        self.__producto = None
+
+    def cantidadTotalProductos(self):
+        print("Cantidad total de productos: ",len(self.catalogo))
+
+    def precioTotalProductos(self):
+        for x in self.catalogo:
+            self.__precio = self.__precio + x.precio
+        print("Precio total de productos: ",self.__precio)
 
 class producto:
     def __init__(self, marca, tipo, precio, precioCuidado = False, primeraNecesidad = False):
@@ -26,28 +50,24 @@ class producto:
         self.primeraNecesidad = primeraNecesidad
 
     def __repr__(self):
-        return "Marca: {}\n Tipo: {}\n Precio: {}\n PrecioCuidado: {}\n Primera necesidad: {}\n".format(self.marca, self.tipo,self.precio, self.precioCuidado, self.primeraNecesidad)
-
-class catalogo:
-    def __init__(self):
-        self.producto = None
-        self.productos = list()
-    
-    def __repr__(self):
-        return "{}".format(self.productos)
-    
-    def agregarProducto(self, marca, tipo, precio, precioCuidado = False, primeraNecesidad = False):
-        self.producto = producto(marca, tipo, precio, precioCuidado, primeraNecesidad)
-        self.productos.append(self.producto)
-        self.producto = None
-
+        return "\nMarca: {}\nTipo: {}\nPrecio: {}\nPrecioCuidado: {}\nPrimera necesidad: {}\n".format(self.marca, self.tipo,self.precio, self.precioCuidado, self.primeraNecesidad)
 
 #CODIGO PRINCIPAL
 
 supermercado1 = supermercado('Carrefour','Av. San Martin 446')
-catalogo1 = catalogo()
-catalogo1.agregarProducto('Serenisima', 'Leche',70,True,True)
-print(catalogo1)
+supermercado1.agregarProducto('Serenisima', 'Leche',70,True,True)
+supermercado1.agregarProducto('Sancor', 'Yogur',90,True,True)
+supermercado1.agregarProducto('Coca Cola', 'Gaseosa',120)
+supermercado1.agregarProducto('Pepsi', 'Gaseosa',130)
+supermercado1.agregarProducto('Paladini', 'Salchicha',35)
+supermercado1.agregarProducto('Blanca Flor', 'Harina',45,True,True)
+supermercado1.agregarProducto('Ace', 'Azucar',60)
+supermercado1.agregarProducto('Ala', "Jabon en Polvo",40)
+print(supermercado1)
+supermercado1.cantidadTotalProductos()
+supermercado1.precioTotalProductos()
+
+
 
 
 
